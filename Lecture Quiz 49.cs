419fa6abbf44198f47074db7b5ac269053ecda66
Johnlouie Gutierrez <johgu.829@gmail.com>
2023-05-25 12:24:40 -0700

    New Cubed and Squared porject answering machine

---
+++ Lecture Quiz 49.cs
@@ -0,0 +1,32 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    
+    {
+        /*  
+        Prompt the user to input an integer then to have it be squared and cubed using 
+
+        
+        */
+        Console.WriteLine("Enter a number");
+        int number = int.Parse(Console.ReadLine());
+        int Squared = Square(number);
+        Console.WriteLine("{0} Squared is {1}",number, Squared);
+        int cubed = Cubed(number);
+        Console.WriteLine("{0} Cubed is {1}",number, cubed);
+
+
+    }
+    public static int Square(int number)
+    {
+        int answer = number*number;
+        return answer;
+
+    }
+    public static int Cubed(int number)
+    {
+
+        int answer = Square(number)*number;
+        return answer;
+    }
+}
